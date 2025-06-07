cf6f3e9dae76279a8cf0d5953d28c5179103176d
Gustavo Zorzi <gu.zorzi@gmail.com>
2025-06-07 18:10:21 -0300

    Add H.265 streaming support

--- unifi/cams/reolink.py
+++ unifi/cams/reolink.py
@@ -140,10 +140,12 @@ class Reolink(UnifiCamBase):
         else:
             fps = self.stream_fps[1]
 
-        return (
-            "-acodec copy -c:v copy -vbsf"
-            f' "h264_metadata=tick_rate={fps * 2}"'
-        )
+        if self.args.video_codec == "h265":
+            bsf = f'-bsf:v "hevc_metadata=tick_rate={fps * 2}"'
+        else:
+            bsf = f'-vbsf "h264_metadata=tick_rate={fps * 2}"'
+
+        return f"-acodec copy -c:v copy {bsf}"
 
     async def get_stream_source(self, stream_index: str) -> str:
         if stream_index == "video1":
