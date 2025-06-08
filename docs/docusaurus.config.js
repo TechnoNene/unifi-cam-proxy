f37f1a39e182f807304332a14a5bebe168aa0645
Gustavo Zorzi <gu.zorzi@gmail.com>
2025-06-08 00:05:11 -0300

    fix doxs to point to about page

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'about',
             position: 'left',
             label: 'Docs',
           },
